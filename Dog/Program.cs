800a4a7b84840e5e3ab3f65afe37fc149734616a
DarStar <merlanor@gmail.com>
2022-09-02 11:31:15 +0300

    pushed all

---
+++ Dog/Program.cs
@@ -0,0 +1,25 @@
+﻿int distance = 10000;
+int speedFirstMan = 6;
+int speedSecMan = 5;
+int speedDog = 13;
+int friend = 1;
+int count =0;
+int T = 0;
+
+while(distance > 10)
+{
+if (friend == 1) 
+{ 
+    T = distance / (speedFirstMan + speedDog);
+     friend = 2;
+    }
+    
+else 
+{
+    T = distance / (speedSecMan + speedDog);
+    friend = 1;
+}
+distance = distance - (speedFirstMan + speedSecMan) * T;
+count++;
+}
+Console.WriteLine(count);
---
+++ ExGreet/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Who is there?");
+string username = Console.ReadLine();
+
+if (username == "Daria")
+   {
+    Console.Write("Hail, ");
+    Console.Write(username);
+   }
+else
+{
+    Console.Write("Hello, ");
+    Console.Write(username);
+}
