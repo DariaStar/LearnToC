31f61284a854dd6b9dfcc6502f5a00f38ccad43b
DarStar <merlanor@gmail.com>
2022-10-02 19:36:47 +0300

    added seminar9

--- Seminar9/Program.cs
+++ Seminar9/Program.cs
@@ -81,3 +81,89 @@
 // Console.WriteLine();
 // }
 // Console.ReadLine();
+
+//Треугольник Паскаля
+
+// Console.Write("Введите число элементов в основании треугольника Паскаля: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int i, c;
+// float factorial(int n)
+// {
+// float i, x = 1;
+// for (i = 1; i <= n; i++)
+// x *= i;
+// return x;
+// }
+
+// for (i = 0; i < n; i++)
+// {
+// for (c = 0; c <= (n - i); c++)
+// Console.Write(" ");
+// for (c = 0; c <= i; c++)
+// {
+// Console.Write(" ");
+// Console.Write(factorial(i) / (factorial(c) * factorial(i - c)));
+// }
+// Console.WriteLine();
+// Console.WriteLine();
+// }
+// Console.ReadLine();
+
+// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+// string PrintNumbers(int n)
+// {
+//     if (n == 1)
+//         return "1";
+//     return PrintNumbers(n - 1) + $" {n}";
+// }
+
+// Console.WriteLine(PrintNumbers(7));
+
+// Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+// Console.WriteLine("Enter M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// string PrintNumbers(int n)
+// {
+//     if (n == m)
+//         return $"{m}";
+//     return PrintNumbers(n - 1) + $" {n}";
+// }
+
+// Console.WriteLine(PrintNumbers(n));
+
+// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+// int PrintNumbers(int n)
+// {
+// if (n < 10)
+// return n;
+// return PrintNumbers(n / 10) + n % 10;
+// }
+
+// Console.WriteLine(PrintNumbers(12));
+
+// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B 
+//с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+// int PrintNumbers(int n, int m)
+// {
+// if (m == 0)
+// return 1;
+// return PrintNumbers(n, m - 1) * n;
+// }
+
+// Console.WriteLine(PrintNumbers(2, 5));
